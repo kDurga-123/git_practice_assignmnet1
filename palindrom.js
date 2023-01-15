0d62fd7eaa426ed07b7faec5d366722517349109
k.durga Nagaraja Reddy <kanchamdurga@gmail.com>
2023-01-15 15:13:55 +0530

    code changed

--- palindrom.js
+++ palindrom.js
@@ -1,7 +1,11 @@
 let string ="madam";
 let bag="";
-for(i=0;i<=string;i--){
-    if(){
-        console.log("bag");
-    }
+for(i=string.length-1;i>=0;i--){
+    bag+=string[i]
+}
+if(string==bag) {
+    console.log("pallindrome")
+}
+else {
+    console.log("not a pallindrome")
 }
