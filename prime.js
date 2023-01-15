cdca4e51751d7201c3baab3d5a2e295f7d2ef372
k.durga Nagaraja Reddy <kanchamdurga@gmail.com>
2023-01-15 14:24:30 +0530

    prime number

---
+++ prime.js
@@ -0,0 +1,13 @@
+let n=5;
+let count=0;
+for(let i=2;i<=n;i++) {
+    if(n%i==0) {
+        count++
+    }
+}
+if(count==1) {
+    console.log("prime number")
+}
+else {
+    console.log("not a prime number")
+}
